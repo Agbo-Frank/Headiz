cfe563025c45d715f69f6d0286d013805418635b
Agbo-Frank <agbofrank531@gmail.com>
2021-08-24 11:33:42 +0100

    improved commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var ejs = require('ejs')
 var router = require('./router')
+const port = process.env.PORT || 5000
 
 var app = express()
 
@@ -10,4 +11,4 @@ app.use('/', express.static('asserts'))
 app.use(router)
 
 
-app.listen(5000)
+app.listen(port)
